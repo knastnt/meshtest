1b051d3e130ceba3ec5012057707d4ddaf4f465d
knastnt <456kot@mail.ru>
2020-08-25 17:21:00 +1000

    basic + formlogin авторизация

---
+++ src/main/java/ru/knastnt/meshtest/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package ru.knastnt.meshtest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.Collection;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+
+        UserDetails theUser = User.withUsername("user")
+                .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder()::encode)
+                .password("1").roles("USER").build();
+
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+
+        userDetailsManager.createUser(theUser);
+
+        return userDetailsManager;
+    }
+
+}

5fc80a1c440821e6a6aaac078bb873452528ca74
knastnt <456kot@mail.ru>
2020-09-01 20:58:25 +1000

    лашарэ ми кантаааре

--- src/main/java/ru/knastnt/meshtest/model/Profile.java
+++ src/main/java/ru/knastnt/meshtest/model/Profile.java
@@ -60,7 +60,7 @@ public class Profile {
     }
 
     public void setEmail(String email) {
-        this.email = email;
+        this.email = email.toLowerCase();
     }
 
     public Integer getAge() {
